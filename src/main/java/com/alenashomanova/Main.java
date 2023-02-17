3cbb47de912eaab99babd6e7fe6dbd593e6f9add
Alena Shomanova <alenashomanova@gmail.com>
2023-02-17 09:53:58 -0400

    Changes the text into the optimized one

--- src/main/java/com/alenashomanova/Main.java
+++ src/main/java/com/alenashomanova/Main.java
@@ -2,6 +2,10 @@ package com.alenashomanova;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA_GURU!");
+        System.out.println(messageName());
+    }
+
+    private static String messageName(){
+        return "This is an optimized message";
     }
 }
