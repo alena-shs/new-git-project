19b0881a232b9fe2240eb2d7f8ee2df279afabc3
Alena <122632153+alena-shs@users.noreply.github.com>
2023-02-17 10:17:29 -0400

    Changed the punctuation

--- src/main/java/com/alenashomanova/Main.java
+++ src/main/java/com/alenashomanova/Main.java
@@ -2,7 +2,7 @@ package com.alenashomanova;
 
 public class Main {
     public static void main(String[] args) {
-        String entity = "QA_GURU";
+        String entity = "QA.GURU";
         System.out.println("Hello "+entity+"!");
     }
 }
