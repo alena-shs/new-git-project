a0a30645e4d0357b35ceed712481990dbb8614d6
Alena <122632153+alena-shs@users.noreply.github.com>
2023-02-17 09:53:02 -0400

    Update Main.java

--- src/main/java/com/alenashomanova/Main.java
+++ src/main/java/com/alenashomanova/Main.java
@@ -2,6 +2,7 @@ package com.alenashomanova;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA_GURU!");
+        String entity = "QA_GURU";
+        System.out.println("Hello "+entity);
     }
 }
